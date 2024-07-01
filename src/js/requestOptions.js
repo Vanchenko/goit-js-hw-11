c678d06e5fdbded21b581ede4b5eb3ccb6ef2e4d
Yevhen <66661914+Vanchenko@users.noreply.github.com>
2024-07-01 16:48:42 +0300

    Update requestOptions.js

--- src/js/requestOptions.js
+++ src/js/requestOptions.js
@@ -3,7 +3,7 @@ export const options = {
     params: {
         key: '35244614-3f1384186f27e7cacc119fb8b',
         page: 1,
-        per_page: 40,
+        per_page: 50,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: true,
